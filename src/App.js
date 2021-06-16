2aa7b5ec88ed12a6e66980327a35acdc2d08d78b
Haris Rashid <62747193+harisrab@users.noreply.github.com>
2021-06-16 18:17:08 +0500

    edited sass

--- src/App.js
+++ src/App.js
@@ -1,55 +1,67 @@
 import React from "react";
-import "./App.scss";
+import "./App.css";
 
 import Header from "./components/Header";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="container">
-          <div className="wrapper">
-            <div className="home">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/opportunities" component={Opportunities} />
-                <Route exact path="/solutions" component={Solutions} />
-                <Route exact path="/contact-us" component={Contact} />
-              </Switch>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<div className="container">
+					<div className="wrapper">
+						<div className="home">
+							<Switch>
+								<Route exact path="/" component={Home} />
+								<Route
+									exact
+									path="/opportunities"
+									component={Opportunities}
+								/>
+								<Route
+									exact
+									path="/solutions"
+									component={Solutions}
+								/>
+								<Route
+									exact
+									path="/contact-us"
+									component={Contact}
+								/>
+							</Switch>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 function Opportunities() {
-  return <p>Discover our numerous opportunities</p>;
+	return <p>Discover our numerous opportunities</p>;
 }
 
 function Solutions() {
-  return <p>Solutions that help you.</p>;
+	return <p>Solutions that help you.</p>;
 }
 
 function Contact() {
-  return <p>Feel free to reach us.</p>;
+	return <p>Feel free to reach us.</p>;
 }
 
 function Home() {
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <h5>
-          The <b>HAMBRG</b>, is a creative, engineer driven, global agency
-          working on advancing the software, advertising and design communities
-          to new heights.
-        </h5>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="wrapper">
+				<h5>
+					The <b>HAMBRG</b>, is a creative, engineer driven, global
+					agency working on advancing the software, advertising and
+					design communities to new heights.
+				</h5>
+			</div>
+		</div>
+	);
 }
 export default App;
